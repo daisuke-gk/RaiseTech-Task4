5de929a22c4984730762730a3498fdec3cd01566
daisuke-gk <daidai481.gake@gmail.com>
2023-10-01 12:21:40 +0900

    init commit

--- main/java/org/example/Main.java
+++ main/java/org/example/Main.java
@@ -11,10 +11,11 @@ public class Main {
         List<String> names = List.of("5", "0", "yasuda", "suzuki", "a", "d", "?", "%", "[", "{");
         List<String> result = names.stream().sorted().toList();
         List<String> result2 = names.stream().map(name -> name.toUpperCase()).toList();
-        List<String> result3 = names.stream().map(name -> name.toUpperCase()).toList();
 
         System.out.println("ソート結果" + result);
         System.out.println("ソート結果" + result2);
+
+        names.stream().sorted().map(String::toUpperCase).forEach(System.out::println);
 
     }
 
